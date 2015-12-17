3cc1cee414988955d5b8d0c8c36198debfdc068a
takahasy <YosukeTakahashi@next-group.jp>
2015-12-17 14:56:15 +0900

    Set margin.

--- components/top_menu.js
+++ components/top_menu.js
@@ -2,12 +2,22 @@
 
 var React = require('react-native');
 var {
-  Text
+  Text,
+  Platform,
+  StyleSheet
 } = React;
 
 var TopMenu = React.createClass({
   render: function() {
-    return <Text>Hello!</Text>
+    return <Text style={styles.container}>Hello!</Text>
+  }
+});
+
+const MARGIN_TOP_IOS = 64;
+const MARGIN_TOP_ANDROID = 0;
+var styles = StyleSheet.create({
+  container: {
+    marginTop: Platform.OS === 'ios' ? MARGIN_TOP_IOS : MARGIN_TOP_ANDROID
   }
 });
 

c23a5cb9734746b4d58f048548eca78df67a1962
takahasy <YosukeTakahashi@next-group.jp>
2015-12-17 12:29:08 +0900

    Add NavigatorIOS

---
+++ components/top_menu.js
@@ -0,0 +1,14 @@
+'user strict';
+
+var React = require('react-native');
+var {
+  Text
+} = React;
+
+var TopMenu = React.createClass({
+  render: function() {
+    return <Text>Hello!</Text>
+  }
+});
+
+module.exports = TopMenu;
--- index.ios.js
+++ index.ios.js
@@ -8,25 +8,20 @@ var React = require('react-native');
 var {
   AppRegistry,
   StyleSheet,
-  Text,
-  View,
+  NavigatorIOS
 } = React;
+var TopMenu = require('./components/top_menu.js');
 
 var ReactNativeSample = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'TopMenu',
+          component: TopMenu,
+        }}
+      />
     );
   }
 });
@@ -34,20 +29,8 @@ var ReactNativeSample = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: 'white',
+  }
 });
 
 AppRegistry.registerComponent('ReactNativeSample', () => ReactNativeSample);

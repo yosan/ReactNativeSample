05041bf8df43c5560df9f6c269d82f871dd3fbba
Yosuke Takahashi <taka.yosuke@gmail.com>
2015-12-18 09:21:23 +0900

    Add tranform sample.

--- components/app/single_view_demo/single_view_demo.js
+++ components/app/single_view_demo/single_view_demo.js
@@ -2,13 +2,52 @@
 
 var React = require('react-native');
 var {
-  Text
+  Text,
+  StyleSheet,
+  View
 } = React;
 
 var SingleViewDemo = React.createClass({
   render: function() {
-    return <Text style={{marginTop: 64}}>Single View Demo!!</Text>
+    return (
+      <View style={styles.container}>
+        <Text>Single View Demo!!</Text>
+        <View style={styles.colored} />
+        <View style={styles.rotated} />
+        <View style={styles.skewed} />
+      </View>
+    )
   }
 });
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 64
+  },
+  colored: {
+    height: 80,
+    width: 100,
+    backgroundColor: "blue"
+  },
+  rotated: {
+    height: 80,
+    width: 100,
+    backgroundColor: "yellow",
+    alignSelf: "center",
+    transform: [
+      {rotate: "45deg"}
+    ]
+  },
+  skewed: {
+    height: 80,
+    width: 100,
+    backgroundColor: "green",
+    transform: [
+      {skewX: "30deg"}
+    ]
+  }
+});
+
 
 module.exports = SingleViewDemo;

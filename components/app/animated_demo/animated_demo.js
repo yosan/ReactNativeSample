8b57284e41dcf77ac889895f0878f50e8696d7ca
Yosuke Takahashi <taka.yosuke@gmail.com>
2015-12-18 09:25:46 +0900

    Add anmated sample.

--- components/app/animated_demo/animated_demo.js
+++ components/app/animated_demo/animated_demo.js
@@ -1,14 +1,45 @@
+/*
+ * This is a sample at here.
+ * https://facebook.github.io/react-native/docs/animations.html
+ */
+
 'user strict';
 
-var React = require('react-native');
+import React from 'react-native';
 var {
-  Text
+  Animated
 } = React;
 
-var AnimatedDemo = React.createClass({
-  render: function() {
-    return <Text style={{marginTop: 64}}>Animated Demo!!</Text>
+class AnimatedDemo extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      bounceValue: new Animated.Value(0),
+    };
   }
-});
+  render(): ReactElement {
+    return (
+      <Animated.Image
+        source={{uri: 'http://i.imgur.com/XMKOH81.jpg'}}
+        style={{
+          flex: 1,
+          transform: [
+            {scale: this.state.bounceValue},
+          ]
+        }}
+      />
+    );
+  }
+  componentDidMount() {
+    this.state.bounceValue.setValue(1.5);
+    Animated.spring(
+      this.state.bounceValue,
+      {
+        toValue: 0.8,
+        friction: 1,
+      }
+    ).start();
+  }
+}
 
 module.exports = AnimatedDemo;
